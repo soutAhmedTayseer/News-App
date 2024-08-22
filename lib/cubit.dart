c209566308afda5d31193f581e20b313df73f597
soutAhmedTayseer <122810273+soutAhmedTayseer@users.noreply.github.com>
2024-08-23 02:25:49 +0300

    change cubit observer date

--- lib/cubit.dart
+++ lib/cubit.dart
@@ -65,7 +65,7 @@ class AppCubit extends Cubit<AppStates> {
     if (sports.isEmpty) {
       DioHelper.getData(url: 'v2/everything', query: {
         'q': 'sports',
-        'from': '2024-07-21',
+        'from': '2024-07-23',
         'sortBy': 'publishedAt',
         'apiKey': '88241b388d4c4ed6939e73c72d6b84d5',
       }).then((value) {
